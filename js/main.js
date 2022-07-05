fff6432758067a6a47f71f9c0f5914106cdc44fe
Valerio <canivalerio93@gmail.com>
2022-07-05 21:14:57 +0200

    fine seconda milestone esercizio

--- js/main.js
+++ js/main.js
@@ -1,34 +1,63 @@
 let prezzo 
-const element = document.getElementById("myBtn");
-element.addEventListener('click', myFunction);
+const create = document.getElementById("myBtn");
+create.addEventListener('click', myFunction);
+
+const cancel = document.getElementById("clear");
+cancel.addEventListener('click', clearFunction);
 
 function myFunction() {
+
     //aquisizione dati e calcolo costanti
     const nameAndSurname = document.getElementById('name').value;
     const kilometriNum = parseInt(document.getElementById('jurneykm').value);
     const etaRange = document.getElementById('eta').value;
     const prezzoPieno = kilometriNum * 0.21;
-    //calcolo prezzi scontati in base all'età
-    switch (etaRange){
-        case "minorenne":
-            prezzo = prezzoPieno - (prezzoPieno * 0.2);
-            break;
-        
-        case "anziano":
-            prezzo = prezzoPieno - (prezzoPieno * 0.4);
-            break;
-        
-        case  "adulto": 
-            prezzo = prezzoPieno;
-            break;
+    const numeroCarrozza = Math.floor(Math.random() * 10);
+    const numeroCodice = Math.floor(Math.random() * 10001) + 90000;
     
-        default:
-            alert("inserire fascia d'età");
+    if (document.getElementById("name").value.length == 0 || document.getElementById("jurneykm").value.length == 0 || document.getElementById("eta").value.length == 0 ) {
+        
+        alert("compilare tutti i campi vuoti");
+        
+    } else {
+
+        //calcolo prezzi scontati in base all'età
+        switch (etaRange){
+            case "minorenne":
+                prezzo = prezzoPieno - (prezzoPieno * 0.2);
+                document.getElementById("offer").innerHTML = "Biglietto Mocciosi";
+                break;
+            
+            case "anziano":
+                prezzo = prezzoPieno - (prezzoPieno * 0.4);
+                document.getElementById("offer").innerHTML = "Biglietto Decrepiti";
+                break;
+            
+            case  "adulto": 
+                prezzo = prezzoPieno;
+                document.getElementById("offer").innerHTML = "Biglietto Normies";
+                break;
+        
+            default:
+                alert("inserire fascia d'età");
+        }
+        
+        document.getElementById("passenger").innerHTML = nameAndSurname;
+        document.getElementById("carriage").innerHTML = numeroCarrozza;
+        document.getElementById("code").innerHTML = numeroCodice;
+        document.getElementById("price").innerHTML = prezzo.toFixed(2) + " euro";
+    
+        document.querySelector('.ticket-creator').classList.remove('HideTicket');
     }
-    console.log("il prezzo pieno del biglietto è di " + prezzoPieno.toFixed(2)  + " euro")
-    console.log("il prezzo del biglietto è di " + prezzo.toFixed(2) + " euro")
 }
 
-
+function clearFunction() {
+    document.getElementById("name").value ="";
+    document.getElementById("jurneykm").value ="";
+    function setSelectValue (id, val) {
+        document.getElementById(id).value = val;
+    }
+    setSelectValue('eta', "");
+}
 
 
