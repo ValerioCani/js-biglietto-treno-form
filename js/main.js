1a9f60a37e97ceae2f310983ada608abae4519b6
Valerio <canivalerio93@gmail.com>
2022-07-05 19:06:58 +0200

    prima parte esercizio completata

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
-const eta = prompt("inserisci la tua età");
-const kilometri = prompt("inserisci quanti kilometri devi percorrere");
+let prezzo 
+const element = document.getElementById("myBtn");
+element.addEventListener('click', myFunction);
 
-const etaNum = parseInt(eta);
-const kilometriNum = parseInt(kilometri);
-
-const prezzoPieno = kilometriNum * 0.21;
-console.log("Il prezzo del biglietto non scontato è: " + prezzoPieno + "euro");
-
-if (etaNum < 18){
-    let prezzo = prezzoPieno - (prezzoPieno * 0.2);
-    alert ( "il prezzo del bilglietto è di " + prezzo.toFixed(2) + " euro." );
-} else if (etaNum > 65){
-    let prezzo = prezzoPieno - (prezzoPieno * 0.4);
-    alert ( "il prezzo del bilglietto è di " + prezzo.toFixed(2) + " euro." );
-} else {
-    let prezzo = prezzoPieno;
-    alert ( "il prezzo del bilglietto è di " + prezzo.toFixed(2) + " euro." );
+function myFunction() {
+    //aquisizione dati e calcolo costanti
+    const nameAndSurname = document.getElementById('name').value;
+    const kilometriNum = parseInt(document.getElementById('jurneykm').value);
+    const etaRange = document.getElementById('eta').value;
+    const prezzoPieno = kilometriNum * 0.21;
+    //calcolo prezzi scontati in base all'età
+    switch (etaRange){
+        case "minorenne":
+            prezzo = prezzoPieno - (prezzoPieno * 0.2);
+            break;
+        
+        case "anziano":
+            prezzo = prezzoPieno - (prezzoPieno * 0.4);
+            break;
+        
+        case  "adulto": 
+            prezzo = prezzoPieno;
+            break;
+    
+        default:
+            alert("inserire fascia d'età");
+    }
+    console.log("il prezzo pieno del biglietto è di " + prezzoPieno.toFixed(2)  + " euro")
+    console.log("il prezzo del biglietto è di " + prezzo.toFixed(2) + " euro")
 }
- 
+
+
+
+
